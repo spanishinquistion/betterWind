b93f6fc38ceab50236a74e8e0807c8768f14bb56
WindMark <44115005+spanishinquistion@users.noreply.github.com>
2020-06-07 13:23:18 +0200

    text for nav bar

---
+++ webfiles/navbar.js
@@ -0,0 +1,6 @@
+var motto = "Making Data Information";
+var navbar_dd = {}
+navbar_dd.tab1 = "Overview";
+navbar_dd.tab2 = "Data";
+navbar_dd.tab3 = "Methods and Information";
+var xxx = "../00003/dashboard_00003.html";
